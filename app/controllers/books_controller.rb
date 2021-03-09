bfd1ef8faac5863d3b49442d9916d556efa9f3ed
Shivang Nagaria <shivang.nagaria@gmail.com>
2021-03-09 21:09:27 +0530

    Added View only BooksController & actions

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,13 @@
+class BooksController < ApplicationController
+  # TODO: Are these action names making sense?
+
+  # Root/Landing page of App
+  def main
+
+  end
+
+  # Page to view all books in inventory & take necessary actions
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  root to: "books#main"
+  get "/books" => "books#index"
+
   namespace :api do
     namespace :v1 do
+      # Remove unnecessary routes??
       resources :books, only: [:index, :create, :update, :destroy]
     end
   end
+
+  # On 400/500, redirect properly
 end
